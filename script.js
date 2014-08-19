f9cd82abdc6434030afc1ef99d9099828758cbc0
Alexander Ose <alex@alexose.com>
2014-08-18 19:43:06 -0700

    scaffold

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+var Generator = function(){
+	this.init();
+	return this;
+};
+
+Generator.prototype.init = function(){
+	return this;
+};
+
+Generator.prototype.templates = {
+
+};
+
+new Generator();
+
+});
