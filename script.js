4510b5e01c7a701c8685a2ec01b4455c244ada4c
Alexander Ose <alex@alexose.com>
2014-08-18 20:23:26 -0700

    syntax highlighting and such

--- script.js
+++ script.js
@@ -6,10 +6,45 @@ var Generator = function(){
 };
 
 Generator.prototype.init = function(){
+
+	this.container = $('body');
+
+	this
+		.initTemplater();
+
 	return this;
 };
 
+// Set up input box
+Generator.prototype.initTemplater = function(){
+
+	$(this.templates.input)
+		.keyup(this.handler.bind(this))
+		.appendTo(this.container);
+
+	return this;
+};
+
+// Handle keydown event
+// TODO: http://stackoverflow.com/questions/14508707/updating-an-inputs-value-without-losing-cursor-position
+Generator.prototype.handler = function(evt){
+
+	var input   = $(evt.target),
+		text    = input.text(),
+		regex   = new RegExp('{{(.+?)}}', 'g');
+		matches = text.match(regex);
+
+	for (var i in matches){
+		var match = matches[i];
+
+		text = text.replace(match, '<span class="highlight">' + match + '</span>');
+	}
+
+	input.html(text);
+};
+
 Generator.prototype.templates = {
+	input : '<div class="fake-input" contentEditable />',
 
 };
 
