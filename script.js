80a06eb36785bacc42a493e486558497b1df0e8f
Alexander Ose <alex@alexose.com>
2014-08-19 20:16:14 -0700

    semi-working

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ var Generator = function(){
 Generator.prototype.init = function(){
 
 	this.container = $('body');
+	this.matches   = [];
+	this.index     = {};
 
 	this
-		.initTemplater();
+		.initTemplater()
+		.initButtons();
 
 	return this;
 };
@@ -18,11 +21,68 @@ Generator.prototype.init = function(){
 // Set up input box
 Generator.prototype.initTemplater = function(){
 
-	$(this.templates.input)
+	this.templater = $(this.templates.input)
 		.keyup(this.handler.bind(this))
-		.appendTo(this.container);
+		.text('<x> bottles of beer on the wall')
+		.appendTo(this.container)
+		.trigger('keyup');
 
 	return this;
+};
+
+Generator.prototype.initButtons = function(){
+
+	this.templater
+		.after(
+			$(this.templates.submit)
+				.click(handle.bind(this))
+		);
+
+	function handle(evt){
+		evt.preventDefault();
+
+		var obj = {};
+		for (var prop in this.index){
+			if (this.index.hasOwnProperty(prop)){
+				obj[prop] = this.index[prop]
+					.find('textarea')
+					.val()
+					.split(',');
+			}
+		}
+
+		this.submit({data : obj});
+	}
+
+	return this;
+};
+
+Generator.prototype.defaults = {
+	google: "https://www.google.com/search?q=",
+	sieve:  "http://sieve.alexose.com?callback=?"
+};
+
+Generator.prototype.submit = function(obj){
+
+	var term = this.templater.text()
+		.replace(/ /g, '+') // replace spaces with pluses
+		.replace('<', '{{') // replace carats with handlebars-esque delimiters
+		.replace('>', '}}');
+
+	obj.url = this.defaults.google + term;
+	obj.selector = '$("body div#resultStats").text()';
+	obj.engine = 'jquery';
+
+	var json = btoa(JSON.stringify(obj));
+
+	$.ajax({
+		url : this.defaults.sieve,
+		data : {json : json},
+		dataType : "jsonp",
+		success : function(result){
+			console.log(result);
+		}
+	});
 };
 
 // Handle keydown event
@@ -32,27 +92,70 @@ Generator.prototype.handler = function(evt){
 	var input     = evt.target,
 		$input    = $(input),
 		text      = $input.text(),
-		regex     = new RegExp('{{(.+?)}}', 'g'),
-		matches   = text.match(regex),
-		selection = window.getSelection(),
-		range     = selection.getRangeAt(0);
+		regex     = new RegExp('<(.+?)>', 'g'),
+		matches   = text.match(regex) || [];
 
-	console.log(range);
-	for (var i in matches){
-		var match = matches[i];
+	var enter  = [],
+		exit   = [],
+		update = [];
 
-		text = text.replace(match, '<span class="highlight">' + match + '</span>');
+	matches = matches.map(strip);
+
+	matches.forEach(function(match){
+		if ($.inArray(match, this.matches) === -1){
+			enter.push(match);
+		} else {
+			update.push(match);
+		}
+	}.bind(this));
+
+	this.matches.forEach(function(match){
+		if ($.inArray(match, matches) === -1){
+			exit.push(match);
+		}
+	});
+
+	this.add(enter);
+	this.hide(exit);
+
+	this.matches = matches;
+
+	function strip(str){
+		return str.substr(1,str.length-2);
 	}
+};
 
-	$input.html(text);
-	console.log(range);
-	window.getSelection().addRange(range);
+// Add or show textareas
+Generator.prototype.add = function(arr){
 
+	arr.forEach(function(str){
+		var el = this.index[str];
+		if (!el){
+			el = this.index[str] = $(
+					this.templates.textarea
+						.replace('{{title}}', str)
+				)
+				.hide()
+				.appendTo(this.container);
+		}
+
+		el.fadeIn();
+
+	}.bind(this));
+};
+
+// Hide textareas
+Generator.prototype.hide = function(arr){
+
+	arr.forEach(function(str){
+		this.index[str].fadeOut();
+	}.bind(this));
 };
 
 Generator.prototype.templates = {
-	input : '<div class="fake-input" contentEditable />',
-
+	input:    '<div class="fake-input" contentEditable />',
+	textarea: '<div class="dynamic-textarea"><h3>{{title}}</h3><textarea></textarea></div>',
+	submit:   '<div class="fake-submit">Search Google</div>'
 };
 
 new Generator();
