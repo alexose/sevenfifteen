41737f9ebc08f947406edd47cbc5415d5e960307
Alexander Ose <alex@alexose.com>
2014-08-21 19:53:15 -0700

    stuck on permutation

--- script.js
+++ script.js
@@ -1,163 +1,240 @@
 $(document).ready(function(){
 
 var Generator = function(){
-	this.init();
-	return this;
+    this.init();
+    return this;
 };
 
 Generator.prototype.init = function(){
 
-	this.container = $('body');
-	this.matches   = [];
-	this.index     = {};
+    this.container = $('#main');
+    this.matches   = [];
+    this.index     = {};
 
-	this
-		.initTemplater()
-		.initButtons();
+    this
+        .initSieve()
+        .initTemplater()
+        .initButtons();
 
-	return this;
+    return this;
+};
+
+// Connect to Sieve
+Generator.prototype.initSieve = function(){
+
+    var server = 'ws://sieve.alexose.com:8082/',
+        ws     = new WebSocket(server);
+
+    ws.onmessage = function(event){
+
+        var str = event.data;
+
+        if (!str){
+            return;
+        }
+
+        data = JSON.parse(str).data;
+
+        if (data){
+            this.display(data);
+        }
+
+    }.bind(this);
+
+    this.ws = ws;
+
+    return this;
+};
+
+Generator.prototype.display = function(){
 };
 
 // Set up input box
 Generator.prototype.initTemplater = function(){
 
-	this.templater = $(this.templates.input)
-		.keyup(this.handler.bind(this))
-		.text('<x> bottles of beer on the wall')
-		.appendTo(this.container)
-		.trigger('keyup');
 
-	return this;
+    this.preview = $(this.templates.preview)
+        .appendTo(this.container);
+
+    this.templater = $(this.templates.input)
+        .keyup(this.handler.bind(this))
+        .text('<x> bottles of beer on the wall')
+        .prependTo(this.container)
+        .trigger('keyup');
+
+
+    return this;
 };
 
 Generator.prototype.initButtons = function(){
 
-	this.templater
-		.after(
-			$(this.templates.submit)
-				.click(handle.bind(this))
-		);
+    this.templater
+        .after(
+            $(this.templates.submit)
+                .click(handle.bind(this))
+        );
 
-	function handle(evt){
-		evt.preventDefault();
+    function handle(evt){
+        evt.preventDefault();
 
-		var obj = {};
-		for (var prop in this.index){
-			if (this.index.hasOwnProperty(prop)){
-				obj[prop] = this.index[prop]
-					.find('textarea')
-					.val()
-					.split(',');
-			}
-		}
+        this.submit({data : obj});
+    }
 
-		this.submit({data : obj});
-	}
-
-	return this;
+    return this;
 };
 
 Generator.prototype.defaults = {
-	google: "https://www.google.com/search?q=",
-	sieve:  "http://sieve.alexose.com?callback=?"
+    google: "https://www.google.com/search?q=",
+    sieve:  "http://sieve.alexose.com?callback=?"
 };
 
 Generator.prototype.submit = function(obj){
 
-	var term = this.templater.text()
-		.replace(/ /g, '+') // replace spaces with pluses
-		.replace('<', '{{') // replace carats with handlebars-esque delimiters
-		.replace('>', '}}');
+    var term = this.templater.text()
+        .replace(/ /g, '+') // replace spaces with pluses
+        .replace('<', '{{') // replace carats with handlebars-esque delimiters
+        .replace('>', '}}');
 
-	obj.url = this.defaults.google + term;
-	obj.selector = '$("body div#resultStats").text()';
-	obj.engine = 'jquery';
+    obj.url = this.defaults.google + term;
+    obj.selector = '$("body div#resultStats").text()';
+    obj.engine = 'jquery';
 
-	var json = btoa(JSON.stringify(obj));
+    var str = JSON.stringify(obj);
+    this.ws.send(str);
 
-	$.ajax({
-		url : this.defaults.sieve,
-		data : {json : json},
-		dataType : "jsonp",
-		success : function(result){
-			console.log(result);
-		}
-	});
 };
 
-// Handle keydown event
+// Handle keyup event
 // TODO: http://stackoverflow.com/questions/14508707/updating-an-inputs-value-without-losing-cursor-position
 Generator.prototype.handler = function(evt){
 
-	var input     = evt.target,
-		$input    = $(input),
-		text      = $input.text(),
-		regex     = new RegExp('<(.+?)>', 'g'),
-		matches   = text.match(regex) || [];
+    var input     = evt.target,
+        $input    = $(input),
+        text      = $input.text(),
+        regex     = new RegExp('<(.+?)>', 'g'),
+        matches   = text.match(regex) || [];
 
-	var enter  = [],
-		exit   = [],
-		update = [];
+    var enter  = [],
+        exit   = [],
+        update = [];
 
-	matches = matches.map(strip);
+    matches = matches.map(strip);
 
-	matches.forEach(function(match){
-		if ($.inArray(match, this.matches) === -1){
-			enter.push(match);
-		} else {
-			update.push(match);
-		}
-	}.bind(this));
+    matches.forEach(function(match){
+        if ($.inArray(match, this.matches) === -1){
+            enter.push(match);
+        } else {
+            update.push(match);
+        }
+    }.bind(this));
 
-	this.matches.forEach(function(match){
-		if ($.inArray(match, matches) === -1){
-			exit.push(match);
-		}
-	});
+    this.matches.forEach(function(match){
+        if ($.inArray(match, matches) === -1){
+            exit.push(match);
+        }
+    });
 
-	this.add(enter);
-	this.hide(exit);
+    this.add(enter);
+    this.hide(exit);
 
-	this.matches = matches;
+    this.matches = matches;
 
-	function strip(str){
-		return str.substr(1,str.length-2);
-	}
+    function strip(str){
+        return str.substr(1,str.length-2);
+    }
+};
+
+// Generate all permutations
+Generator.prototype.update = function(evt){
+
+    var titles = [],
+        arrays = [],
+        template = this.templater.text();
+
+    for (var prop in this.index){
+        if (this.index.hasOwnProperty(prop)){
+            arrays.push(
+                this.index[prop]
+                    .find('textarea')
+                    .val()
+                    .split(',')
+            );
+
+            titles.push(prop);
+        }
+    }
+
+    var permutation = permutations(obj);
+
+    function permutations(titles, arrays){
+        var obj = {};
+
+        (function recurse(arr){
+            for (var i in arr){
+                var subarray = arr[i],
+                    title = titles[i],
+                    temp = [];
+
+            }
+        })(arrays);
+    }
+
+    console.log(combinations);
+    this.preview.text();
 };
 
 // Add or show textareas
 Generator.prototype.add = function(arr){
 
-	arr.forEach(function(str){
-		var el = this.index[str];
-		if (!el){
-			el = this.index[str] = $(
-					this.templates.textarea
-						.replace('{{title}}', str)
-				)
-				.hide()
-				.appendTo(this.container);
-		}
+    arr.forEach(function(str){
+        var el = this.index[str];
+        if (!el){
+            el = this.index[str] = $(
+                    this.templates.textarea
+                        .replace('{{title}}', str)
+                )
+                .keyup(this.update.bind(this))
+                .hide()
+                .appendTo(this.container);
+        }
 
-		el.fadeIn();
+        el.fadeIn();
 
-	}.bind(this));
+    }.bind(this));
 };
 
 // Hide textareas
 Generator.prototype.hide = function(arr){
 
-	arr.forEach(function(str){
-		this.index[str].fadeOut();
-	}.bind(this));
+    arr.forEach(function(str){
+        this.index[str].fadeOut();
+    }.bind(this));
 };
 
 Generator.prototype.templates = {
-	input:    '<div class="fake-input" contentEditable />',
-	textarea: '<div class="dynamic-textarea"><h3>{{title}}</h3><textarea></textarea></div>',
-	submit:   '<div class="fake-submit">Search Google</div>'
+    input:    '<div class="fake-input" contentEditable />',
+    textarea: '<div class="dynamic-textarea"><h3>{{title}}</h3><textarea></textarea></div>',
+    submit:   '<div class="fake-submit">Search Google</div>',
+    preview:   '<span class="preview" />'
 };
 
 new Generator();
 
 });
+
+// Via http://stackoverflow.com/questions/4331092
+function allPossibleCases(arr) {
+  if (arr.length == 1) {
+    return arr[0];
+  } else {
+    var result = [];
+    var allCasesOfRest = allPossibleCases(arr.slice(1));  // recur with the rest of array
+    for (var i = 0; i < allCasesOfRest.length; i++) {
+      for (var j = 0; j < arr[0].length; j++) {
+        result.push(arr[0][j] + allCasesOfRest[i]);
+      }
+    }
+    return result;
+  }
+
+}
